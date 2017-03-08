923fa032a05cf86c5323699b20c3765eb331f3c1
Vaguesei <theo.Vleeschouwers@eleve.ensai.fr>
2017-03-08 15:00:30 +0100

    Update make_trace_bis

--- src/make_trace_bis.py
+++ src/make_trace_bis.py
@@ -18,3 +18,16 @@ while i < len(sorted_history.index.values.tolist()):
     print(i)
   
 regroup_history = sorted_history[["buyer_id","timestamp","event"]][sorted_history["regroup"]==0]
+
+with open(cmd_folder+"data/processed/trace_regroup.json","w") as buf:
+    buf.write("[")
+    buyers = regroup_history["buyer_id"].unique()
+    for buyer in buyers[0:buyers.size-1]:
+        buf.write("{\"id\":\""+buyer+"\",\"trace\":")
+        regroup_history[regroup_history["buyer_id"]==buyer][["event"]].to_json(path_or_buf=buf,orient="records",force_ascii=False)
+        buf.write("},\n")
+    buf.write("{\"id\":\""+buyer+"\",\"trace\":")
+    regroup_history[regroup_history["buyer_id"]==buyer][["event"]].to_json(path_or_buf=buf,orient="records",force_ascii=False)
+    buf.write("}]")
+
+trace = json.load(open(cmd_folder+"data/processed/trace_regroup.json","r"))
