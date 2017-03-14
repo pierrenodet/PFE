0ef01c5b36bfd5532cdb7b525c82d5d7c48d7b78
PierreNODET <nodet.pierre@gmail.com>
2017-03-14 18:07:35 +0100

    Maj de model_density.py pour exporter les resultats obtenus dans /output

--- src/make_data.py
+++ src/make_data.py
@@ -7,3 +7,7 @@ os.makedirs(cmd_folder+"data/raw/", exist_ok=True)
 os.makedirs(cmd_folder+"data/interim/", exist_ok=True)
 os.makedirs(cmd_folder+"data/processed/", exist_ok=True)
 print("Mettez les export_ensai_buyersHistory_170216.csv et export_ensai_buyersProfil_170216.csv et PFEensai2017_description_des_donnees.xlsx dans PFE/data/raw/")
+
+os.makedirs(cmd_folder+"output/picture/", exist_ok=True)
+os.makedirs(cmd_folder+"output/data/", exist_ok=True)
+#ensai_2017 FTP
--- src/model_density.py
+++ src/model_density.py
@@ -3,6 +3,11 @@ from datetime import timedelta
 from find_dir import cmd_folder
 from clean_data import stringlist_to_datelist
 
+from numpy import linspace
+from scipy.stats import gaussian_kde
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
 ##Importing data
 
 buyer_history=pd.read_csv(cmd_folder+"data/processed/buyer_history.csv")
@@ -40,3 +45,42 @@ density_percentage["timestamp_percentage"]=(density_percentage["timestamp"]-dens
 density_percentage.drop(density_percentage[density_percentage["event"]=="quote_lose"].index.values,inplace=True)
 
 density_percentage.to_csv(cmd_folder+"data/processed/density_percentage.csv",index=False)
+
+xs=linspace(0,1,1000)
+kernels=[[]*density_percentage["event"].unique().size]*xs.size
+for i,event in enumerate(density_percentage["event"].unique()):
+    kernels[i] = gaussian_kde(density_percentage["timestamp_percentage"][density_percentage["event"]==event].dropna())(xs)
+
+max_events = [0]*xs.size
+for x in range(xs.size):
+    maxi=0
+    for i in range(density_percentage["event"].unique().size):
+        if maxi < kernels[i][x]:
+            maxi = kernels[i][x]
+            max_events[x] = density_percentage["event"].unique()[i]
+
+max_event = max_events[0]
+breaks = [0]
+limit = 0
+for x in range(xs.size):
+    limit +=1
+    if max_event != max_events[x]:
+        if limit > xs.size*0.2:
+            limit = 0
+            max_event = max_events[x]
+            breaks.append(x)
+        else:
+            max_event = max_events[x]
+
+colors = [ cm.jet(x) for x in linspace(0.0,1.0, density_percentage["event"].unique().size) ]
+
+with open(cmd_folder+"output/data/breaks.txt",'w') as breaks_file:
+    breaks_file.write('\n'.join(str(b/xs.size) for b in breaks))
+
+plt.gcf().clear()
+for i,event in enumerate(density_percentage["event"].unique()):
+    (density_percentage["timestamp_percentage"][density_percentage["event"]==event]).plot(kind="density",use_index=False,label=event,figsize=(10,8),c=colors[i]).set_xlim(0,1)
+plt.legend(loc=2,prop={'size':6})
+for b in breaks[1:len(breaks)]:
+    plt.axvline(b/xs.size,linestyle="--")
+plt.savefig(cmd_folder+"output/picture/density_breaks.svg",bbox_inches='tight')
