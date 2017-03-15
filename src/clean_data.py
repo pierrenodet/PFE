a90b4dcdec18f63bc64df6623eafb4eb3e60eae1
PierreNODET <nodet.pierre@gmail.com>
2017-03-15 19:58:31 +0100

    Recuperation du gros csv sur le ftp, separation du csv en acheteur et non acheteur, nettoyage des donnes comme reunion 13 mars

--- src/clean_data.py
+++ src/clean_data.py
@@ -4,11 +4,11 @@ from find_dir import cmd_folder
 
 ##Importing Data
 
-buyer_history=pd.read_csv(cmd_folder + "data/raw/export_ensai_buyersHistory_170216.csv",sep="\t",header=None)
+history=pd.read_csv(cmd_folder + "data/raw/export_ensai_history_170307170002.csv",sep="\t",header=None)
 
 ##Cleaning Data
 
-buyer_history.columns=["buyer_id","visit_id","timestamp","event","status"]
+history.columns=["buyer_id","visit_id","timestamp","event","status"]
 
 def stringlist_to_datelist(stringlist):
     datelist=[0]*stringlist.size
@@ -16,8 +16,25 @@ def stringlist_to_datelist(stringlist):
         datelist[i] = datetime.strptime(string.split(".")[0],"%Y-%m-%d %H:%M:%S")
     return datelist
 
-buyer_history["timestamp"]=stringlist_to_datelist(buyer_history["timestamp"])
+history["timestamp"]=stringlist_to_datelist(history["timestamp"])
 
-buyer_history.drop_duplicates(inplace=True)
+history.drop_duplicates(inplace=True)
 
-buyer_history.to_csv(cmd_folder+"data/interim/buyer_history_noduplicate.csv",index=False)
+group=pd.read_excel(cmd_folder+"data/raw/PFEensai2017_description_des_donnees.xlsx")
+group.rename(columns={'event_name':'event','group_name':'group'},inplace=True)
+history.loc[:,"event"]=history[["event","status"]].reset_index().merge(group,on=["event","status"]).set_index('index')["group"]
+history.drop("status",axis=1,inplace=True)
+
+non_buyer_history = pd.DataFrame(columns=["buyer_id","visit_id","timestamp","event"])
+buyer_history = pd.DataFrame(columns=["buyer_id","visit_id","timestamp","event"])
+
+buyer_history = pd.merge(history,pd.DataFrame(history[history["event"]=="project_win"]["buyer_id"].values,columns=["buyer_id"]),how="inner",on="buyer_id",right_index=True)
+
+non_buyer_history = history.drop(buyer_history.index)
+
+non_buyer_history.to_csv(cmd_folder+"data/interim/non_buyer_history.csv",index=False)
+
+buyer_history.to_csv(cmd_folder+"data/interim/buyer_history.csv",index=False)
+
+
+#buyer_history=pd.read_csv(cmd_folder + "data/raw/export_ensai_buyersHistory_170216.csv",sep="\t",header=None)
--- src/explore_data.py
+++ src/explore_data.py
@@ -1,18 +1,22 @@
 import pandas as pd
 from find_dir import cmd_folder
+from clean_data import stringlist_to_datelist
+from datetime import timedelta
 
 ## Important modifications of data resulting from exploration
 
-buyer_history=pd.read_csv(cmd_folder+"data/interim/buyer_history_noduplicate.csv")
+buyer_history=pd.read_csv(cmd_folder+"data/interim/buyer_history.csv")
 
 buyer_history = buyer_history.loc[list(buyer_history.loc[:,["buyer_id","timestamp","event"]].drop_duplicates().index),:]
 
-mec_chelou = buyer_history.groupby("buyer_id")["event"].count().idxmax()
-buyer_history = buyer_history[buyer_history["buyer_id"]!=mec_chelou].reset_index(drop=True)
+buyer_history.drop(pd.merge(buyer_history, pd.DataFrame(buyer_history.groupby("buyer_id")["event"].count().sort_values(ascending=False)[0:4].index), on=["buyer_id"],right_index=True).index,inplace=True)
 
-group=pd.read_excel(cmd_folder+"data/raw/PFEensai2017_description_des_donnees.xlsx")
-group.rename(columns={'event_name':'event','group_name':'group'},inplace=True)
-buyer_history.loc[:,"event"]=buyer_history[["event","status"]].reset_index().merge(group,on=["event","status"]).set_index('index')["group"]
-buyer_history.drop("status",axis=1,inplace=True)
+buyer_history["timestamp"]=stringlist_to_datelist(buyer_history["timestamp"])
+
+diff_ts = (buyer_history.groupby("buyer_id")["timestamp"].max()-buyer_history.groupby("buyer_id")["timestamp"].min()).apply(timedelta.total_seconds)
+buyer_history = pd.merge(buyer_history,pd.DataFrame(diff_ts[diff_ts>86400].index),on="buyer_id",how="inner")
+
+len_trace = buyer_history.groupby("buyer_id").size()
+buyer_history = pd.merge(buyer_history,pd.DataFrame(len_trace[len_trace>3].index),on="buyer_id",how="inner")
 
 buyer_history.to_csv(cmd_folder+"data/processed/buyer_history.csv",index=False)
