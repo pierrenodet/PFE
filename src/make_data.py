ff354ee306d2e0e06f20ef61c003cc1dfb33d793
Jérémy <jeremy.le-gall@hotmail.fr>
2017-03-15 11:02:03 +0100

    Modif pour le ftp pour que ça marche, modif sur le sample file du coup aussi

--- src/make_data.py
+++ src/make_data.py
@@ -2,8 +2,9 @@ import os
 from find_dir import cmd_folder
 import paramiko
 import yaml
+import pandas as pd
 
-with open(cmd_folder+"config.yml", 'r') as ymlfile:
+with open(cmd_folder+"config.sample.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
 #Here will be put the script needed to download data from a future database or script to get data from other sources
@@ -15,13 +16,13 @@ os.makedirs(cmd_folder+"data/processed/", exist_ok=True)
 print("Mettez les export_ensai_buyersHistory_170216.csv et export_ensai_buyersProfil_170216.csv et PFEensai2017_description_des_donnees.xlsx dans PFE/data/raw/")
 
 transport = paramiko.Transport((cfg["host"],cfg["port"]))
-transport.connect(cfg["user"],cfg["pwd"])
+transport.connect(username = cfg["user"],password = cfg["pwd"])
 sftp = paramiko.SFTPClient.from_transport(transport)
-if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_history_170307170002.csv")
+if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_history_170307170002.csv"):
     sftp.get("/export_ensai_history_170307170002.csv",cmd_folder+"data/raw/export_ensai_history_170307170002.csv")
 #if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_history_170307170002.headers")
 #    sftp.get("/export_ensai_history_170307170002.headers",cmd_folder+"data/raw/export_ensai_history_170307170002.headers")
-if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_user_170307170002.csv")
+if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_user_170307170002.csv"):
     sftp.get("/export_ensai_user_170307170002.csv",cmd_folder+"data/raw/export_ensai_user_170307170002.csv")
 #if ~os.path.isfile(cmd_folder+"data/raw/export_ensai_user_170307170002.headers")
 #    sftp.get("/export_ensai_user_170307170002.headers",cmd_folder+"data/raw/export_ensai_user_170307170002.headers")
