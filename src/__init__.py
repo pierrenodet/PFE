d2a3aca8996a09e64b831b871081ab106540d18b
PierreNODET <nodet.pierre@gmail.com>
2017-03-27 17:09:49 +0200

    correction de bugs

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,10 @@
+import os, sys, inspect
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+cmd_folder = cmd_folder[0:cmd_folder.find("src")]
+sys.path.append(cmd_folder)
+sys.path.append(cmd_folder+"src/")
+
 import make_data
 import pre_process_data
 import clean_data
--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,4 @@
-import os, sys, inspect
-
 def main(args=None):
-
-    ##Finding project dir
-
-    cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-    cmd_folder = cmd_folder[0:cmd_folder.find("src")]
-    sys.path.append(cmd_folder)
-    sys.path.append(cmd_folder+"src/")
 
     import descriptive_statistics
     import kgrams_CAH
